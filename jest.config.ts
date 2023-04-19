4c74e5c70ba6bf49033a5368ef3b3076c17f3982
Josh Leyshon <joshualeyshon@gmail.com>
2023-04-19 16:02:42 +1000

    Fix jest config, now only runs tests in android/ios environments by default, and web environment only for .test.web|node.ts files

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,14 @@
 import type { Config } from 'jest';
 
 const config: Config = {
-  preset: 'jest-expo/universal',
+  projects: [
+    { preset: 'jest-expo/android' },
+    { preset: 'jest-expo/ios' },
+    {
+      preset: 'jest-expo/web',
+      testMatch: ['**/__tests__/**/*.test.(web|node).ts?(x)'],
+    },
+  ],
   setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
 };
 
