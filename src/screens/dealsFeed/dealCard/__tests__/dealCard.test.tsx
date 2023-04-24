b6fc6439cdceb26270caa195375feb32cf37733c
Josh Leyshon <joshualeyshon@gmail.com>
2023-04-24 11:12:18 +1000

    Add TODO to get deal card image to click on as well

--- src/screens/dealsFeed/dealCard/__tests__/dealCard.test.tsx
+++ src/screens/dealsFeed/dealCard/__tests__/dealCard.test.tsx
@@ -9,6 +9,9 @@ test('Pressing anywhere on the card fires onPress', () => {
     <DealCard title="title" description={descriptionText} onPress={onPress} />,
   );
 
+  // TODO: Figure out how to get the image to click on as well.
+  // `getBy` with all accessibility functions doesn't pick up the <Image> component.
+  // testID works but then seems to show up even when no image is given to the card.
   const description = screen.getByText(descriptionText);
   const card = screen.getByTestId(DEAL_CARD_TEST_ID);
 
