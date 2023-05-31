ea60407aadcd31eb5f324cb3d0e88d5b730ff1ce
Josh Leyshon <45578963+josh-leyshon@users.noreply.github.com>
2023-05-31 14:58:17 +1000

    Add @testing-library/react-native devDep, and initial unit tests for DealCard component (#20)

--- jest.config.ts
+++ jest.config.ts
@@ -1,3 +1,15 @@
-export default {
-  preset: 'jest-expo/universal',
+import type { Config } from 'jest';
+
+const config: Config = {
+  projects: [
+    { preset: 'jest-expo/android' },
+    { preset: 'jest-expo/ios' },
+    {
+      preset: 'jest-expo/web',
+      testMatch: ['**/__tests__/**/*.test.(web|node).ts?(x)'],
+    },
+  ],
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
 };
+
+export default config;
---
+++ src/screens/dealsFeed/dealCard/__tests__/dealCard.test.tsx
@@ -0,0 +1,34 @@
+import { render, screen, fireEvent } from '@testing-library/react-native';
+import { DealCard, DEAL_CARD_TEST_ID } from '../dealCard';
+
+test('Renders', () => {
+  render(
+    <DealCard
+      title="my title"
+      description="my description"
+      onPress={jest.fn()}
+    />,
+  );
+
+  expect(screen.toJSON()).toMatchSnapshot();
+});
+
+test('Pressing anywhere on the card fires onPress', () => {
+  const onPress = jest.fn();
+  const descriptionText = 'desc';
+
+  render(
+    <DealCard title="title" description={descriptionText} onPress={onPress} />,
+  );
+
+  // TODO: Figure out how to get the image to click on as well.
+  // `getBy` with all accessibility functions doesn't pick up the <Image> component.
+  // testID works but then seems to show up even when no image is given to the card.
+  const description = screen.getByText(descriptionText);
+  const card = screen.getByTestId(DEAL_CARD_TEST_ID);
+
+  fireEvent.press(description);
+  fireEvent.press(card);
+
+  expect(onPress).toHaveBeenCalledTimes(2);
+});
