9d43115d22697a1a29ddcd7263c893e1fcd9edc8
Josh Leyshon <joshualeyshon@gmail.com>
2023-07-15 15:17:28 +0930

    Move the View surrounding base Button component into the base component, so the buttons will expand to fill available container space by default wherever they're used

--- src/base/components/button/button.tsx
+++ src/base/components/button/button.tsx
@@ -1,12 +1,12 @@
-import { Button as ReactNativeButton } from 'react-native';
+import { Button as ReactNativeButton, View, StyleSheet } from 'react-native';
 import type { ButtonProps as ReactNativeButtonProps } from 'react-native';
 
 const colors = {
   orange: '#ffbd59',
   green: '#82f152',
-  lightgreen: '#beffa2',
+  lightGreen: '#beffa2',
   red: '#f15e5e',
-  lightred: '#ffb7b7',
+  lightRed: '#ffb7b7',
 } as const;
 
 type Colors = keyof typeof colors;
@@ -20,6 +20,18 @@ type ButtonProps = {
 export function Button({ color, title, onPress }: ButtonProps): JSX.Element {
   const buttonColor = colors[color];
   return (
-    <ReactNativeButton title={title} onPress={onPress} color={buttonColor} />
+    // TODO: Could use a custom Pressable component to add styles, instead of a Button wrapped in a View.
+    <View style={styles.container}>
+      <ReactNativeButton title={title} onPress={onPress} color={buttonColor} />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    // Buttons should expand to fill available container space by default.
+    // Larger buttons are better for mobile touch targets.
+    flexBasis: 1,
+    flexGrow: 1,
+  },
+});
