e71e0c707ff00e26e4a28d03efa33cde21aa0bd3
Josh Leyshon <joshualeyshon@gmail.com>
2025-01-09 07:14:26 +0000

    Fix links overlapping following text on Android
    Signed-off-by: Josh Leyshon <joshualeyshon@gmail.com>

--- src/base/components/text/link.tsx
+++ src/base/components/text/link.tsx
@@ -3,12 +3,12 @@ import type React from 'react';
 import { useState } from 'react';
 import { Platform, Pressable, StyleSheet } from 'react-native';
 import { colours } from '../../constants/colours';
-import { fontSizes } from '../../constants/text';
 import { Row } from '../../layout/flex';
 import type { TextProps } from './text';
 import { Text } from './text';
 
-export type LinkProps = Omit<TextProps, 'colour'> & {
+export type LinkProps = Omit<TextProps, 'children' | 'colour'> & {
+  children: string;
   onPress: () => void;
 };
 
@@ -19,20 +19,28 @@ export type LinkProps = Omit<TextProps, 'colour'> & {
 export function Link({ onPress, children, ...props }: LinkProps): React.JSX.Element {
   const [pressed, setPressed] = useState(false);
 
+  // I noticed that only on Android, Text following the View would not have adequate spacing.
+  // The end of the view and the start of the Text would be touching or overlapping.
+  // This padding was added to help that. The value was chosen manually after checking what looks good enough.
+  // I however have no idea _why_ this value is the right one to look good enough.
+  //
+  // It also expects that the Text next to this Link will have a space " " first,
+  // because my TextPart algorithm should do that.
+  const paddingEnd = children.length * 0.75;
+
   return (
     <Pressable
       onPressIn={() => setPressed(true)}
       onPressOut={() => setPressed(false)}
       onPress={onPress}
-      style={Platform.OS === 'android' ? styles.pressableAndroid : undefined}
+      style={Platform.OS === 'android' ? [styles.pressableAndroid, { paddingEnd }] : undefined}
     >
-      <Row gap='small' alignItems='center'>
+      <Row alignItems='center'>
         <Text {...props} colour={pressed ? 'primaryLight' : 'primaryDark'}>
           {children}
         </Text>
         <Icon
           name='arrow-outward'
-          size={fontSizes.medium}
           color={pressed ? colours.primaryLight : colours.primaryDark}
         />
       </Row>
@@ -43,7 +51,7 @@ export function Link({ onPress, children, ...props }: LinkProps): React.JSX.Elem
 const styles = StyleSheet.create({
   pressableAndroid: {
     // Nesting Views within Text on Android will add extra bottom margin/something to the View,
-    // which you can't get rid of. This makes the text within the view render higher than the surrounding text.
+    // which you can't get rid of. This makes the text/items within the View render higher than the surrounding text.
     //
     // This workaround moves the view down by a fixed amount that roughly aligns the View with the Text.
     // It's not perfect and should be replaced when this issue is fixed:
