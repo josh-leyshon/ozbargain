4187f7d7d54d8cfb53973e47a7bfd77271339619
Josh Leyshon <joshualeyshon@gmail.com>
2025-01-10 02:00:21 +0000

    Update DealsFeed screen to support using Top or New deals feeds
    Signed-off-by: Josh Leyshon <joshualeyshon@gmail.com>

--- src/screens/dealsFeed/screen.tsx
+++ src/screens/dealsFeed/screen.tsx
@@ -5,22 +5,36 @@ import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { colours } from '../../base/constants/colours';
 import { sizes } from '../../base/constants/sizes';
 import { useDealsFeed } from '../../global-state/dealsFeed';
+import type { FeedScreenProps } from '../navigationRoutes';
 import { DealsFeed } from './dealsFeed';
 
-export function FeedScreen(): React.JSX.Element {
+export function FeedScreen({ route }: FeedScreenProps): React.JSX.Element {
   const insets = useSafeAreaInsets();
   const navigation = useNavigation();
-  const { state, dealsFeed, refreshNewDeals, loadNewDealsNextPage } = useDealsFeed();
+  const { state, dealsFeed, refreshTopDeals, refreshNewDeals, loadTopDealsNextPage, loadNewDealsNextPage } =
+    useDealsFeed();
+
+  const { feedType } = route.params;
+
+  const getDeals = feedType === 'top'
+    ? () => dealsFeed?.getTopDeals() ?? []
+    : () => dealsFeed?.getNewDeals() ?? [];
+  const refreshDeals = feedType === 'top'
+    ? refreshTopDeals
+    : refreshNewDeals;
+  const loadNextPage = feedType === 'top'
+    ? loadTopDealsNextPage
+    : loadNewDealsNextPage;
 
   return (
     <DealsFeed
       style={[styles.container, {
         paddingBlockStart: insets.top + sizes.medium,
       }]}
-      deals={dealsFeed?.getNewDeals() ?? []}
+      deals={getDeals()}
       onPressItem={item => navigation.navigate('DealInfo', { dealId: item.id })}
-      onRefresh={refreshNewDeals}
-      loadNextPage={() => state === 'ready' ? loadNewDealsNextPage() : undefined}
+      onRefresh={refreshDeals}
+      loadNextPage={() => state === 'ready' ? loadNextPage() : undefined}
       refreshing={state === 'refreshing'}
     />
   );
