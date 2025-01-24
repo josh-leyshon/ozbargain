2c4ff545807822371a38fc6081af5d76ddc61c90
Josh Leyshon <joshualeyshon@gmail.com>
2025-01-24 09:16:37 +0000

    remove react-native-cheerio and use an RC release of actual cheerio that works on react native instead. yay!
    Signed-off-by: Josh Leyshon <joshualeyshon@gmail.com>

--- src/deal-info-scraper/webScraper.ts
+++ src/deal-info-scraper/webScraper.ts
@@ -1,24 +1,19 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable @typescript-eslint/no-unsafe-return */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-import { writeFile } from 'node:fs/promises';
-import { join } from 'node:path';
-import cheerio, { type DollarResult } from 'react-native-cheerio';
+// import { writeFile } from 'node:fs/promises';
+// import { join } from 'node:path';
+import { load } from 'cheerio';
 
 const url = 'https://www.ozbargain.com.au/node/888965';
 
 function getCoupons(html: string): string[] {
   const couponsDiv = html.match(/(?:<div class="couponcode.*?\/div>)/)?.[0];
-  const couponStrongs = couponsDiv?.matchAll(/(?:<strong>(.+?)<\/strong>)/g);
-  if (!couponStrongs) {
+  const couponStrings = couponsDiv?.matchAll(/(?:<strong>(.+?)<\/strong>)/g);
+  if (!couponStrings) {
     console.warn('Found no coupons');
     return [];
   }
 
   const coupons: string[] = [];
-  for (const match of couponStrongs) {
+  for (const match of couponStrings) {
     const couponText = match.at(1);
     if (!couponText) {
       console.warn('Found coupon <strong> element but no text within');
@@ -30,16 +25,11 @@ function getCoupons(html: string): string[] {
   return coupons;
 }
 
-function getCommentsFromThread(thread: DollarResult) {
-  const comment = thread.children('div.comment-wrap');
-  console.log(comment.text());
-}
-
 function getComments(html: string) {
-  const $ = cheerio.load(html);
+  const $ = load(html);
   const level0Comments = $('ul.comment.level0 > li');
 
-  return level0Comments.map((_: any, elem: any) => {
+  return level0Comments.map((_, elem) => {
     return $('> div.comment-wrap div.content', elem).text();
   }).get();
 }
