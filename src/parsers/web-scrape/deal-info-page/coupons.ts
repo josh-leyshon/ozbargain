5037772941192a2e83128ec075ef889f2bba9b4d
Josh Leyshon <joshualeyshon@gmail.com>
2025-01-27 06:47:16 +0000

    replace coupon code scraping regex code with cheerio
    Signed-off-by: Josh Leyshon <joshualeyshon@gmail.com>

--- src/parsers/web-scrape/deal-info-page/coupons.ts
+++ src/parsers/web-scrape/deal-info-page/coupons.ts
@@ -1,26 +1,13 @@
+import { load } from 'cheerio';
+
 /**
  * Extract the coupons on a deal info page, if it has any.
  * @param html The HTML of a deal info page.
  */
 export function getDealCouponsFromDocument(html: string): string[] {
-  const couponsDiv = html.match(/(?:<div class="couponcode.*?\/div>)/)?.[0];
-  const couponStrings = couponsDiv?.matchAll(/(?:<strong>(.+?)<\/strong>)/g);
-  if (!couponStrings) {
-    return [];
-  }
+  const $ = load(html);
 
-  const coupons: string[] = [];
-  for (const match of couponStrings) {
-    const couponText = match.at(1);
-    if (!couponText) {
-      console.warn(
-        'Found coupon <strong> element but no text within. Ignoring this coupon. Deal info html:',
-        couponsDiv,
-      );
-      continue;
-    }
-    coupons.push(couponText);
-  }
+  const coupons = $('div.couponcode strong').map((_, elem) => $(elem).text()).get();
 
   return coupons;
 }
