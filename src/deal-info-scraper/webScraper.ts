a7ab3e4ab34e015391d1ea17eee9760c4d04788d
Josh Leyshon <joshualeyshon@gmail.com>
2025-01-27 03:44:38 +0000

    Finish scraping comments with support for threads
    Signed-off-by: Josh Leyshon <joshualeyshon@gmail.com>

--- src/deal-info-scraper/webScraper.ts
+++ src/deal-info-scraper/webScraper.ts
@@ -1,7 +1,9 @@
+import type { CheerioAPI, Element, SelectorType } from 'cheerio';
 import { load } from 'cheerio';
 import { readFile, writeFile } from 'node:fs/promises';
 import { join } from 'node:path';
-import type { PartedText } from '../feed-parser/textParts';
+import { OZBARGAIN_BASE_URL } from '../base/constants/urls';
+import { type PartedText, partText } from '../feed-parser/textParts';
 
 type Comment = {
   /**
@@ -20,13 +22,6 @@ type Comment = {
    */
   votes: number;
   /**
-   * If a comment is downvoted, it is displayed on the site greyer than others.
-   * The "level" of greyness is in the html.
-   * It seems to literally match the number of downvotes, from -1 to -3.
-   * A higher value here means "greyer".
-   */
-  deEmphasis?: 1 | 2 | 3;
-  /**
    * It seems the times in HTML are always same timezone, maybe AEST?
    * The times can also be relative, eg "36mins ago".
    *
@@ -44,7 +39,7 @@ type Comment = {
   children?: Comment[];
 };
 
-const url = 'https://www.ozbargain.com.au/node/888965';
+const url = `${OZBARGAIN_BASE_URL}/node/888965`;
 
 function getCoupons(html: string): string[] {
   const couponsDiv = html.match(/(?:<div class="couponcode.*?\/div>)/)?.[0];
@@ -67,13 +62,57 @@ function getCoupons(html: string): string[] {
   return coupons;
 }
 
+function getCommentAndChildren(rootCheerio: CheerioAPI, comment: Element, level: number): Comment {
+  const $ = (query: SelectorType) => rootCheerio(query, comment);
+
+  const id = $('> div.comment-wrap').attr()?.['id']?.match(/comment-(\d+)/)?.at(1);
+  const timestampMs = Number.parseInt($('> div.comment-wrap > div.comment').attr()?.['data-ts'] ?? '0') * 1000;
+  if (!id) {
+    throw new Error(`Did not find ID for comment. HTML:\n${$('> div.comment-wrap').html()}`);
+  }
+  if (timestampMs === 0) {
+    throw new Error(`Did not find ID for comment. HTML:\n${$('> div.comment-wrap').html()}`);
+  }
+
+  const userATag = $('> div.comment-wrap div.submitted a');
+  const userName = userATag.text();
+  const userLink = userATag.attr()?.href;
+  const userThumbnailUrl = $('> div.comment-wrap div.n-left > img').attr()?.src;
+
+  if (!userLink) {
+    throw new Error(`Did not find comment author profile link for comment. HTML:\n${$('> div.comment-wrap').html()}`);
+  }
+  if (!userThumbnailUrl) {
+    throw new Error(`Did not find comment author thumbnail URL for comment. HTML:\n${$('> div.comment-wrap').html()}`);
+  }
+
+  const votes = Number.parseInt($('> div.comment-wrap span.cvc').text() || '0');
+
+  const contentDiv = $('> div.comment-wrap div.content');
+  const contentHtml = contentDiv.html()?.trim() ?? '';
+
+  const children = $('> ul.comment > li').map((_, elem) => getCommentAndChildren(rootCheerio, elem, level + 1)).get();
+
+  return {
+    id,
+    timestamp: new Date(timestampMs),
+    content: partText(contentHtml),
+    user: {
+      name: userName,
+      thumbnailUrl: userThumbnailUrl,
+      profileUrl: `${OZBARGAIN_BASE_URL}${userLink}`,
+    },
+    votes,
+    level,
+    children,
+  };
+}
+
 function getComments(html: string) {
   const $ = load(html);
   const level0Comments = $('ul.comment.level0 > li');
 
-  return level0Comments.map((_, elem) => {
-    return $('> div.comment-wrap div.content', elem).text();
-  }).get();
+  return level0Comments.map((_, elem) => getCommentAndChildren($, elem, 0)).get();
 }
 
 export async function main() {
@@ -85,7 +124,7 @@ export async function main() {
   console.log(coupons);
 
   const comments = getComments(html);
-  console.log(comments);
+  console.dir(comments, { depth: null });
 }
 
 void main();
