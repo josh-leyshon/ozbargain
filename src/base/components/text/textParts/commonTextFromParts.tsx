497160a217ed9b36d74d35aa760ae1ed6fca1569
Josh Leyshon <joshualeyshon@gmail.com>
2025-02-03 09:10:19 +0000

    allow CommonTextFromParts to accept some Text props
    Signed-off-by: Josh Leyshon <joshualeyshon@gmail.com>

--- src/base/components/text/textParts/commonTextFromParts.tsx
+++ src/base/components/text/textParts/commonTextFromParts.tsx
@@ -3,18 +3,19 @@ import type { TextPart } from '../../../../parsers/text/textParts';
 import { openLink } from '../../../links/openLink';
 import { share } from '../../../links/share';
 import { Link } from '../link';
+import type { TextProps } from '../text';
 import { Text } from '../text';
 
 type CommonTextFromPartsProps = {
   textParts: TextPart[];
-};
+} & Pick<TextProps, 'size' | 'weight' | 'colour'>;
 
 /**
  * Most commonly displayed text can be rendered in the same way,
  * ie. we care to display the same kinds of text parts.
  * This component handles that.
  */
-export function CommonTextFromParts({ textParts }: CommonTextFromPartsProps): React.JSX.Element {
+export function CommonTextFromParts({ textParts, size, weight, colour }: CommonTextFromPartsProps): React.JSX.Element {
   return (
     <Text>
       {textParts.map(part => {
@@ -24,11 +25,22 @@ export function CommonTextFromParts({ textParts }: CommonTextFromPartsProps): Re
               key={part.startIndex}
               onPress={() => openLink(part.url)}
               onLongPress={() => share({ message: part.url })}
+              size={size}
+              weight={weight}
             >
               {part.text}
             </Link>
           )
-          : <Text key={part.startIndex}>{part.text}</Text>;
+          : (
+            <Text
+              key={part.startIndex}
+              size={size}
+              weight={weight}
+              colour={colour}
+            >
+              {part.text}
+            </Text>
+          );
       })}
     </Text>
   );
